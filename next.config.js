d616a52970a6953b446e07fb18f4dfbfcd9132cf
Amit Gangwar <gangwaramit449@gmail.com>
2024-08-29 17:03:22 +0530

    Resolving issues

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 // next.config.js
 module.exports = {
   images: {
-    domains: ["linknestt-by-amit.vercel.app"],
     domains: [
       "firebasestorage.googleapis.com",
       "lh3.googleusercontent.com",
@@ -9,6 +8,7 @@ module.exports = {
       "freight.cargo.site",
       "cloud.headwayapp.co",
       "s3-us-west-2.amazonaws.com",
+      "linknestt-by-amit.vercel.app",
     ],
   },
 };
