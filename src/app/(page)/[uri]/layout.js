a80f5bdc33ab011d6f86426466ac1956ee15349a
Amit Gangwar <142745081+Xerox563@users.noreply.github.com>
2024-11-07 18:11:20 +0530

    Updated layout.js

--- src/app/(page)/[uri]/layout.js
+++ src/app/(page)/[uri]/layout.js
@@ -6,7 +6,7 @@ const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] })
 
 export const metadata = {
   title: 'LinkNest : Organize, Share, Impress',
-  description: 'Store Your Digital Data at Sinlge Place ',
+  description: 'Store Your Digital Data at Single Place ',
 }
 
 export default function RootLayout({ children }) {
